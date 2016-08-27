5cf37f7bdc7ac1727cb5da4ea94e26440b2338e9
Ramkumar68 <ramkumr.kulriya@metacube.com>
2016-08-27 14:09:58 +0530

    marker on map

--- www/components/dashboard/dashboardCtrl.js
+++ www/components/dashboard/dashboardCtrl.js
@@ -2,12 +2,32 @@
  * Created by dharmendra on 8/8/16.
  */
 app.controller('DashboardCtrl', function($scope, $ionicModal, $timeout,leafletData) {
-    $scope.doInit = function() {
-        leafletData.getMap('map1').then(function(map) {
-            $scope.map = map;
-            $log.info(map);
-            $log.info(map);
-        });
-    }
+    $scope.mark={
+            osloMarker: {
+                lat: 59.91,
+                lng: 10.75,
+                message: "I want to travel here!",
+                focus: false,
+                draggable: false
+            },
+            SimpleMarker: {
+            	lat: 26.47297,
+                lng: 75.49,
+                message: "I want here!",
+                focus: false,
+                draggable: false	
+            }
+        }
 
+    angular.extend($scope, {
+        center: {
+            lat: 59.91,
+            lng: 10.75,
+            zoom: 2
+        },//Array of markers to add marker
+        markers: $scope.mark,
+        defaults: {
+            scrollWheelZoom: false
+        }
+    });
 });
