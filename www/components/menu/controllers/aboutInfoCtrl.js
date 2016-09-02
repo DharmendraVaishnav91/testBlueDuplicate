568314ebd059f48989180045792a42fc897e1bef
Ramkumar68 <ramkumr.kulriya@metacube.com>
2016-09-02 18:46:56 +0530

    Terms and Conditon page

--- www/components/menu/controllers/aboutInfoCtrl.js
+++ www/components/menu/controllers/aboutInfoCtrl.js
@@ -1,7 +1,32 @@
-app.controller('AboutInfoCtrl', function($scope,$state) {
+app.controller('AboutInfoCtrl', function($scope,$state,$ionicModal) {
 
 
-        // Open the login modal
-        
-
+    // Open the login modal
+    var openModalType={
+        terms:1,
+    };
+    $ionicModal.fromTemplateUrl('components/menu/views/terms.html', {
+	    scope: $scope,
+	    animation: 'slide-in-right'
+		}).then(function (modal) {
+		    $scope.terms= modal;
     });
+	$scope.openModal = function (modalType) {
+	    switch (modalType) {
+	        case openModalType.terms:
+	                $scope.terms.show();
+	            break;
+	    }
+    };
+    $scope.closeModal = function (modalType) {
+        switch (modalType) {
+            case openModalType.terms:
+                $scope.terms.hide();
+                break;
+        }
+    };
+    $scope.goToTerms=function(){
+    	$scope.openModal(openModalType.terms);
+    };
+
+});
