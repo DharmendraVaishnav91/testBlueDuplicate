aa82fbcde645b7247340598baf42320c33c49197
Dharmendra Vaishnav <dharmendra.vaishnav@metacube.com>
2016-10-03 18:07:03 +0530

    Map bug fixes in language selection

--- www/components/menu/controllers/languageCtrl.js
+++ www/components/menu/controllers/languageCtrl.js
@@ -1,8 +1,8 @@
 menu.controller('LanguageCtrl', function($scope,$state,loginService,$localStorage,$rootScope,menuService,$translate,$window,$cordovaToast) {
     $scope.changeLanguage= function (selectedLang) {
-      console.log("inside LanguageCtrl")
+      console.log("inside LanguageCtrl");
        menuService.selectLanguage(selectedLang).then(function (response) {
-         console.log($rootScope.language)
+         console.log($rootScope.language);
            $translate.use(selectedLang);
            $rootScope.language = selectedLang;
            $cordovaToast.showLongBottom("Language preference updated successfully");
