d7164bae94ed6e43c8d1eba278b7356806cdd027
Dharmendra Vaishnav <dharmendra.vaishnav@metacube.com>
2016-12-01 13:03:30 +0530

    Merge branch 'org' of https://github.com/S3plus2/BlueMobile into org
    # Conflicts:
    #	www/app.js
    #	www/components/common/utils/constants.js
    #	www/components/setting/controllers/organizationCtrl.js
    #	www/components/setting/views/organization.html

--- www/components/organization/controllers/orgMembersCtrl.js
+++ www/components/organization/controllers/orgMembersCtrl.js
@@ -1,6 +1,74 @@
 /**
  * Created by dharmendra on 30/11/16.
  */
-org.controller('OrganizationMembersCtrl', function () {
+org.controller('OrganizationMembersCtrl', function ($scope) {
     console.log("welcome to org tab view controller");
+    $scope.memberList=[
+        {
+            name:"Dharmendra vaishnav",
+            id:"1",
+            role:"admin"
+        },
+        {
+            name:"Amit Swami",
+            id:"2",
+            role:"Member"
+        },
+        {
+            name:"Kapil sachdev",
+            id:"3",
+            role:"Member"
+        },
+
+        {
+            name:"sachin singh",
+            id:"4",
+            role:"Member"
+        },
+        {
+            name:"Manish sharma",
+            id:"5",
+            role:"Member"
+        },
+        {
+            name:"Abha Bansal",
+            id:"6",
+            role:"Member"
+        },
+        {
+            name:"Prateek Dhingra",
+            id:"7",
+            role:"Member"
+        },
+        {
+            name:"Deepak Sharma",
+            id:"8",
+            role:"Member"
+        },
+        {
+            name:"Parag Mangal",
+            id:"9",
+            role:"Member"
+        },
+        {
+            name:"amit natani",
+            id:"10",
+            role:"Member"
+        },
+        {
+            name:"Aahutosh shukla",
+            id:"11",
+            role:"Member"
+        },
+        {
+            name:"poonam dasani",
+            id:"12",
+            role:"Member"
+        },
+        {
+            name:"Sameer sharma",
+            id:"13",
+            role:"Member"
+        }
+    ];
 });
--- www/components/setting/controllers/organizationCtrl.js
+++ www/components/setting/controllers/organizationCtrl.js
@@ -1,4 +1,3 @@
-menu.controller('OrganizationCtrl', function($scope, $filter, $state, utilityService, $translate, $window, $cordovaToast,$ionicModal){
 menu.controller('OrganizationCtrl', function($scope, $filter, $state, $stateParams, loginService, $localStorage, $rootScope, menuService, utilityService, $translate, $window, $cordovaToast){
 
     // $state
@@ -10,74 +9,7 @@ menu.controller('OrganizationCtrl', function($scope, $filter, $state, $statePara
         $scope.memberListModal= modal;
 
     });
-    $scope.memberList=[
-        {
-            name:"Dharmendra vaishnav",
-            id:"1",
-            role:"admin"
-        },
-        {
-            name:"Amit Swami",
-            id:"2",
-            role:"Member"
-        },
-        {
-            name:"Kapil sachdev",
-            id:"3",
-            role:"Member"
-        },
 
-        {
-            name:"sachin singh",
-            id:"4",
-            role:"Member"
-        },
-        {
-            name:"Manish sharma",
-            id:"5",
-            role:"Member"
-        },
-        {
-            name:"Abha Bansal",
-            id:"6",
-            role:"Member"
-        },
-        {
-            name:"Prateek Dhingra",
-            id:"7",
-            role:"Member"
-        },
-        {
-            name:"Deepak Sharma",
-            id:"8",
-            role:"Member"
-        },
-        {
-            name:"Parag Mangal",
-            id:"9",
-            role:"Member"
-        },
-        {
-            name:"amit natani",
-            id:"10",
-            role:"Member"
-        },
-        {
-            name:"Aahutosh shukla",
-            id:"11",
-            role:"Member"
-        },
-        {
-            name:"poonam dasani",
-            id:"12",
-            role:"Member"
-        },
-        {
-            name:"Sameer sharma",
-            id:"13",
-            role:"Member"
-        }
-    ];
     $scope.imgUrl="http://lorempixel.com/30/30/people/";
     console.log($rootScope.orgExist);
     console.log($rootScope.org);
