5d7a1ff94172d840978e429e58517d6723c7fc39
Dharmendra Vaishnav <dharmendra.vaishnav@metacube.com>
2016-12-05 14:41:48 +0530

    Org new features:
     -  Searchable member list.
     -  Invitations history.

--- www/components/organization/controllers/orgDetailCtrl.js
+++ www/components/organization/controllers/orgDetailCtrl.js
@@ -4,31 +4,4 @@
 org.controller('OrganizationDetailCtrl', function ($scope, orgService) {
     console.log("welcome to org tab view controller");
 
-    orgService.fetchOrgInfo().then(function (response) {
-        $scope.detailsData = response.data.attributes;
-        console.log("Responce from server\n",response);
-    }).catch(function (error) {
-        console.log(error);
-    });
-
-    // $scope.detailsData = {
-    //      "owner_name": 'Keshav Sharma',
-    //      "description": 'Software Company',
-    //      "identifier": 'Meta',
-    //      "identifier_type": 'Unknown',
-    //      "name": 'Metacube Softwares',
-    //      "organization_type": 'IT',
-    //      "actor_attributes": {
-    //          "location_attributes": {
-    //            "name": 'Office',
-    //            "latitude": "22",
-    //            "longitude": "-103",
-    //            "address": 'Sitapura',
-    //            "subdivision_code": 'RJ',
-    //            "country_code": 'IN',
-    //            "postalcode": 302022
-    //          },
-    //          "picture_attributes": {"image": "blank"}
-    //      }
-    //    };
 });
--- www/components/organization/controllers/orgTabCtrl.js
+++ www/components/organization/controllers/orgTabCtrl.js
@@ -2,6 +2,13 @@
  * Created by dharmendra on 30/11/16.
  */
 var org=angular.module('app.org',[]);
-org.controller('OrganizationTabCtrl', function () {
+org.controller('OrganizationTabCtrl', function (orgService,$scope) {
    console.log("welcome to org tab view controller");
+   orgService.fetchOrgInfo().then(function (response) {
+      $scope.detailsData = response.data;
+
+      console.log("Responce from server\n",response);
+   }).catch(function (error) {
+      console.log(error);
+   });
 });
