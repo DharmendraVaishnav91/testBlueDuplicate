d3610d7e28943a5d5f0b42ac98705bce60b9a621
Parag Mangal <parag.mangal@metacube.com>
2016-12-05 14:37:25 +0530

    edit Org details and invite org member

--- www/components/organization/controllers/orgDetailCtrl.js
+++ www/components/organization/controllers/orgDetailCtrl.js
@@ -1,16 +1,27 @@
 /**
  * Created by dharmendra on 30/11/16.
  */
-org.controller('OrganizationDetailCtrl', function ($scope, orgService) {
+org.controller('OrganizationDetailCtrl', function ($scope, $state, $actionButton, orgService, $rootScope) {
     console.log("welcome to org tab view controller");
 
     orgService.fetchOrgInfo().then(function (response) {
         $scope.detailsData = response.data.attributes;
+        $rootScope.orgDetail = response;
         console.log("Responce from server\n",response);
     }).catch(function (error) {
         console.log(error);
     });
-
+    var actionButton = $actionButton.create({
+        mainAction: {
+            icon: 'ion-edit',
+            backgroundColor: '#4E5C6E',
+            textColor: ' white',
+            onClick: function() {
+                console.log('clicked edit BUTTON');
+                $state.go('app.createOrg');
+            }
+        }
+    });
     // $scope.detailsData = {
     //      "owner_name": 'Keshav Sharma',
     //      "description": 'Software Company',
