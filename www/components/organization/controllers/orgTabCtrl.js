6beccba2c29ad5f8275f625e3c260ff9360e3fd6
Parag Mangal <parag.mangal@metacube.com>
2016-12-07 17:55:56 +0530

    BLUEN-477

--- www/components/organization/controllers/orgTabCtrl.js
+++ www/components/organization/controllers/orgTabCtrl.js
@@ -4,6 +4,7 @@
 var org=angular.module('app.org',[]);
 org.controller('OrganizationTabCtrl', function (orgService,$scope,$rootScope) {
    console.log("welcome to org tab view controller");
+   $rootScope.defaultImageURL = "https://cdn0.iconfinder.com/data/icons/human-resources-and-strategy/80/Human_resource_strategy-07-512.png"
    orgService.fetchOrgInfo().then(function (response) {
       $scope.detailsData = response.data;
       $rootScope.orgDetail = response;
